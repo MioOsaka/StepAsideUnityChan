b4bc5abe38e23690eb752b0d80598a571208e18c
大坂 美緒 <mosaka@ths.co.jp>
2018-10-08 14:31:17 +0900

    Lesson6課題です

--- Assets/DeleteItem.cs
+++ Assets/DeleteItem.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DeleteItem : MonoBehaviour {
+	private GameObject mainCamera;
 
-	void OnBecameInvisible()
-	{
-		Destroy(this.gameObject);
+	void Start () {
+		this.mainCamera = GameObject.Find ("Main Camera");
 	}
+
+	void Update () {
+		if(mainCamera.transform.position.z > this.transform.position.z){
+			Destroy (this.gameObject);
+		}
+	}
+
+
+
+//	void OnBecameInvisible()
+//	{
+//		Destroy(this.gameObject);
+//	}
 }
